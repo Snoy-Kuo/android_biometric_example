2bd0bc7c35d6b55df49a80cd136b2a56f894c847
Snoy Kuo <snoykuo@gmail.com>
2022-03-08 16:47:56 +0800

    create project. fix warnings, reformat code. add viewbinding support.

---
+++ app/src/main/java/com/snoy/biometric_example/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.snoy.biometric_example
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.snoy.biometric_example.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/snoy/biometric_example/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.snoy.biometric_example.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
